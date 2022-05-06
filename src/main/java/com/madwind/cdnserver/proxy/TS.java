8f09df67194f0f500714a3f45dbf894d7fda1cb7
madwind <madwind.cn@gmail.com>
2022-05-06 22:46:47 +0800

    init

--- src/main/java/com/madwind/cdnserver/proxy/TS.java
+++ src/main/java/com/madwind/cdnserver/proxy/TS.java
@@ -1,18 +1,17 @@
 package com.madwind.cdnserver.proxy;
 
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ZeroCopyHttpOutputMessage;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 public class TS implements ProxyResponse {
     private final String urlParam;
@@ -31,29 +30,36 @@ public class TS implements ProxyResponse {
 
     @Override
     public Mono<ServerResponse> handle() {
-        Flux<DataBuffer> dataBufferFlux = WebClient.builder()
-                                                   .clientConnector(new ReactorClientHttpConnector(
-                                                           HttpClient.create().followRedirect(true)
-                                                   ))
-                                                   .baseUrl(urlParam)
-                                                   .exchangeStrategies(ExchangeStrategies.builder()
-                                                                                         .codecs(configurer ->
-                                                                                                 configurer.defaultCodecs()
-                                                                                                           .maxInMemorySize(maxInMemorySize)
-                                                                                         )
-                                                                                         .build())
-                                                   .build()
-                                                   .get()
-                                                   .retrieve().bodyToFlux(DataBuffer.class);
-        return ServerResponse.ok()
-                             .contentType(getMediaType())
-                             .body((p, a) -> {
-                                         ZeroCopyHttpOutputMessage resp = (ZeroCopyHttpOutputMessage) p;
-                                         HttpHeaders headers = resp.getHeaders();
-                                         headers.setLastModified(ZonedDateTime.now());
-                                         headers.setCacheControl(ProxyResponse.CACHE_CONTROL);
-                                         return resp.writeWith(dataBufferFlux);
-                                     }
-                             );
+        return WebClient.builder()
+                        .clientConnector(new ReactorClientHttpConnector(
+                                HttpClient.create().followRedirect(true)
+                        ))
+                        .baseUrl(urlParam)
+                        .exchangeStrategies(ExchangeStrategies.builder()
+                                                              .codecs(configurer ->
+                                                                      configurer.defaultCodecs()
+                                                                                .maxInMemorySize(maxInMemorySize)
+                                                              )
+                                                              .build())
+                        .build()
+                        .get()
+                        .retrieve()
+                        .toEntityFlux(DataBuffer.class)
+                        .flatMap(fluxResponseEntity -> ServerResponse.ok()
+                                                                     .contentType(getMediaType())
+                                                                     .headers(httpHeaders -> {
+                                                                         httpHeaders.setLastModified(ZonedDateTime.now());
+                                                                         httpHeaders.setCacheControl(ProxyResponse.CACHE_CONTROL);
+                                                                         httpHeaders.setContentType(fluxResponseEntity.getHeaders()
+                                                                                                                      .getContentType());
+                                                                         httpHeaders.setContentLength(fluxResponseEntity.getHeaders()
+                                                                                                                        .getContentLength());
+                                                                     })
+                                                                     .body((p, a) -> {
+                                                                                 ZeroCopyHttpOutputMessage resp = (ZeroCopyHttpOutputMessage) p;
+                                                                                 return resp.writeWith(Objects.requireNonNull(fluxResponseEntity.getBody()));
+                                                                             }
+                                                                     )
+                        );
     }
 }
